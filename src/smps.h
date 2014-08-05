c5d71d348cff17b1605c6d9b8937f5d016abfcbb
Kira McCallum <kirasglimmer@gmail.com>
2014-08-04 19:03:13 -0700

    Refactoring globals

--- src/smps.h
+++ src/smps.h
@@ -18,11 +18,13 @@
 #define PWM_OUTPUT	PB1
 #define ADC_INPUT	PB4
 
+const uint8_t CYCLE_CLK     = 250;
+const uint8_t CYCLE_MAX	    = (CYCLE_CLK * 0.70)
+const uint8_t TARGET_ADC	= 74;
 
-#define CYCLE_CLK	250
-#define CYCLE_MAX	(CYCLE_CLK * 0.70)
-#define TARGET_ADC	74
-
+const uint8_t RAMP_DELAY    = 10;
+const uint8_t RAMP_START    = 5;
+const uint8_t RAMP_STEPS    = CYCLE_CLK / RAMP_DELAY;
 
 
 #endif
